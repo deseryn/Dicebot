fcc2a73eb9ec6d2b3279b6b19569935452e09f69
deseryn <lang.christian1995@gmail.com>
2022-01-29 12:12:08 +0100

    Add struct and more ideas

--- src/main.go
+++ src/main.go
@@ -5,3 +5,16 @@ import "fmt"
 func main() {
 	fmt.Println("success")
 }
+
+type Hero struct{
+	Name string `json:"name"`
+	Class string `json:"class"`
+	Race string `json:"race"`
+	Gender string `json:"gender"`
+	Weight float64 `json:"weight"`
+	Height float64 `json:"height"`
+	Hp int `json:"hp"`
+	Money float64 `json:"monye"`
+	Attributes map[string]int `json:"attributes"`
+	Talents map[string]int `json:"talents"`
+}
