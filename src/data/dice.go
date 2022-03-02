d240a310205a3484e397c4429deb6093e69a2d06
justeige <38982396+justeige@users.noreply.github.com>
2022-03-02 19:44:58 +0100

    add very basic dice rolling (no sums or modifier)

--- src/data/dice.go
+++ src/data/dice.go
@@ -1,3 +1,25 @@
 package data
 
-// TODO add functions for dice rolling
+import (
+	"encoding/json"
+	"io"
+	"math/rand"
+)
+
+type DiceRolls []int
+
+func Roll(faces int, count int) DiceRolls {
+	rolls := []int{}
+
+	for i := 0; i < count; i++ {
+		roll := rand.Intn(faces-1) + 1
+		rolls = append(rolls, roll)
+	}
+
+	return rolls
+}
+
+func (dr *DiceRolls) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(dr)
+}
--- src/routes/dice.go
+++ src/routes/dice.go
@@ -1,3 +1,45 @@
 package routes
 
+import (
+	"dicebot/data"
+
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
 // TODO add api for dice rolls
+
+type Dice struct {
+	l *log.Logger
+}
+
+func NewDiceRoute(l *log.Logger) *Dice {
+	return &Dice{l}
+}
+
+func (dr *Dice) RollDice(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	face, err := strconv.Atoi(vars["face"])
+	if err != nil {
+		http.Error(rw, "Unable to convert 'face'", http.StatusBadRequest)
+		return
+	}
+
+	count, err := strconv.Atoi(vars["count"])
+	if err != nil {
+		http.Error(rw, "Unable to convert 'count'", http.StatusBadRequest)
+		return
+	}
+
+	diceRolls := data.Roll(face, count)
+
+	dr.l.Println("Roll d", face, count, "times: ", diceRolls)
+
+	err = diceRolls.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
