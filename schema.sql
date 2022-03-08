708b4abb032a0ddc762086ba030e13c084393d55
deseryn <lang.christian1995@gmail.com>
2022-03-08 21:21:22 +0100

    Add db create script and docker yml

---
+++ schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE IF NOT EXISTS `heroes` (
+	`name` VARCHAR(50) NOT NULL,
+	`race` VARCHAR(30),
+	`class` VARCHAR(30),
+	`gender` VARCHAR(30),
+	`weight` FLOAT,
+	`height` FLOAT,
+    `max_hp` INT,
+	`current_hp` INT,
+	`money` FLOAT,
+	PRIMARY KEY (`name`)
+);
+
+CREATE TABLE IF NOT EXISTS `attributes` (
+	`name` VARCHAR(50) NOT NULL,
+	`description` text
+	PRIMARY KEY (`name`)
+);
+
+CREATE TABLE IF NOT EXISTS `talents` (
+	`name` VARCHAR(50) NOT NULL,
+    `attr1` VARCHAR(50) NOT NULL,
+	`attr2` VARCHAR(50) NOT NULL,
+	`attr3` VARCHAR(50) NOT NULL,
+	`description` text
+	PRIMARY KEY (`name`)
+);
+
+CREATE TABLE `hero_attributes` (
+	`hero` VARCHAR(50) NOT NULL,
+	`attribute` VARCHAR(50) NOT NULL,
+	`value` INT NOT NULL,
+	PRIMARY KEY (`hero`,`attribute`)
+);
+
+CREATE TABLE `hero_talents` (
+	`hero` VARCHAR(50) NOT NULL,
+	`talent` VARCHAR(50) NOT NULL,
+	`value` INT NOT NULL,
+	PRIMARY KEY (`hero`,`talent`)
+);
