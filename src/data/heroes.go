248b64d527bd1338faac8628dc34df1b9d6278ed
Christian Lang <christian.lang@flyeralarm.com>
2022-03-31 16:56:38 +0200

    Add first select/put methods

--- src/data/heroes.go
+++ src/data/heroes.go
@@ -1,8 +1,11 @@
 package data
 
 import (
+	"dicebot/db"
 	"encoding/json"
 	"io"
+	"log"
+	"github.com/antonholmquist/jason"
 )
 
 type Hero struct {
@@ -11,6 +14,7 @@ type Hero struct {
 	Hp         int            `json:"hp"`
 	Race       string         `json:"race"`
 	Gender     string         `json:"gender"`
+	Class      string         `json:"class"`
 	Weight     float64        `json:"weight"`
 	Height     float64        `json:"height"`
 	Money      float64        `json:"money"`
@@ -27,27 +31,114 @@ func (h *Heroes) ToJSON(w io.Writer) error {
 }
 
 func GetHeroes() Heroes {
-	// TODO this should come from a db
-	return dbMock
+	heroList = nil
+	db, err := db.GetDBConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM heroes")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		var h Hero
+
+		err = rows.Scan(&h.Name, &h.MaxHp, &h.Hp, &h.Race, &h.Gender, &h.Class, &h.Weight, &h.Height, &h.Money)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		attJSON, _ := jason.NewObjectFromBytes([]byte(att))
+		h.Attributes = attJSON
+		talJSON, _ := jason.NewObjectFromBytes([]byte(tal))
+		h.Talents = talJSON
+
+		heroList = append(heroList, &h)
+	}
+
+	return heroList
 }
 
-var dbMock = Heroes{
-	&Hero{
-		ID:     1,
-		Name:   "Jago",
-		MaxHp:  40,
-		Hp:     40,
-		Race:   "human",
-		Gender: "male",
-		Money:  9999,
-	},
-	&Hero{
-		ID:     2,
-		Name:   "Estor",
-		MaxHp:  29,
-		Hp:     20,
-		Race:   "human",
-		Gender: "male",
-		Money:  10,
-	},
+func GetHeroByName(name string) *Hero {
+	db, err := db.GetDBConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var h Hero
+	var att string
+	var tal string
+	err = db.QueryRow("SELECT * FROM heroes WHERE name=?", name).Scan(&h.Name, &h.MaxHp, &h.Hp, &h.Race, &h.Gender, &h.Weight, &h.Height, &h.Money, &att, &tal)
+	if err != nil {
+		log.Fatal(err)
+	}
+	attJSON, _ := jason.NewObjectFromBytes([]byte(att))
+	h.Attributes = attJSON
+	talJSON, _ := jason.NewObjectFromBytes([]byte(tal))
+	h.Talents = talJSON
+
+	heroList = append(heroList, &h)
+
+	return &h
 }
+
+func GetHeroField(name string, field string) interface{} {
+	// if attributes/talents contains field -> get attribute/talent
+	
+	db, err := db.GetDBConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var f string
+	err = db.QueryRow("SELECT ? FROM heroes WHERE name=?", field, name).Scan(&f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return f
+}
+
+func GetHeroAttribute(name string, attribute string) jason.Object {
+	db, err := db.GetDBConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var a string
+	var v string
+	err = db.QueryRow("SELECT attribute, value FROM hero_attributes WHERE name=? AND attribute = ?", name, attribute).Scan(&f, &v)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// make json response
+}
+
+func SetHeroField(name string, field string, value interface{}) {
+	db, err := db.GetDBConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	db.QueryRow("UPDATE heroes SET ? = ? WHERE name = ?", field, value, name)
+}
+
+var heroList []*Hero
+attributes[8] := [
+	"Mut",
+	"Klugheit",
+	"Intuition",
+	"Geschicklichkeit",
+	"Charisma",
+	"Fingerfertigkeit",
+	"Konstitution",
+	"Körperkraft"
+]
